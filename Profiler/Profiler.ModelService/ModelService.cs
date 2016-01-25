e953689ac32d9e2802038c39e28a229b405fd8d7
kosachev <5616906@mail.ru>
2016-01-25 22:35:31 +0300

    my second commit added IModelService ModelService

---
+++ Profiler/Profiler.ModelService/ModelService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiler.ModelService
+{
+   public abstract class ModelService:IModelService
+    {
+       public int Key { get; set; }
+
+       public string Name { get; set; }
+    }
+}
